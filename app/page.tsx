5e12042c48f7c5808605b6c8d1aa91900a313ced
austinschroeder <austinsoundy@gmail.com>
2023-06-21 14:40:39 -0700

    begin landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Image from 'next/image';
+'use client';
+import { Card, Text, Subtitle, Divider } from '@tremor/react';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Lets Build a GPT-4 Weather App</h1>
-    </main>
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to [#183b73]">
+      <Card>
+        <Text className="text-6xl font-bold text-center mb-10">Weather AI</Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 13.3, Tailwind CSS, Tremor 2.0 + More!
+        </Subtitle>
+
+        <Divider className="my-10" />
+        <Card className="bg-gradient-to-br from-[#394F68] to [#183b73]">
+          {/* CityPicker  */}
+        </Card>
+      </Card>
+    </div>
   );
 }
